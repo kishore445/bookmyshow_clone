92df0f99d58d05524bf5d5ba80676fb744278d3c
Kishore <kishorekishore201@gmail.com>
2025-03-08 12:26:57 +0530

    Register API integrated with client

---
+++ client/src/APICalls/user.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+    headers:{
+        credentials:"include",
+        method: "post",
+        "Content-Type": "application/json",
+    }
+});
+
+export const registerUser = async (payload)=>{
+    try{
+        const response = await axiosInstance.post("/app/v1/users/register",payload);
+            return response;
+        
+    }catch(err){
+        return err;
+    }
+}
--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -1,36 +1,44 @@
 import React from "react";
-import { Form } from "antd";
+import { Form,  message } from "antd";
 import Button from "../../Componenets/Button";
-// import {Link } from "react-router-dom";
 import { Link } from 'react-router-dom';
+import { registerUser } from "../../APICalls/user";
+
 
 const Register = () => {
+  const onFinish= async (values)=>{
+    try{
+      const response = await registerUser(values);
+      if(response.success){
+        message.sucess(response.message);
+      }
+    }catch(err){
+      message.error(err);
+    }
+  }
   return (
     <div className="flex justify-center h-screen items-center bg-main">
 
    
     <div className="card p-3 w-400">
       <h1>Welcome to BookMyShow</h1>
-      <Form layout="vertical">
+      <Form layout="vertical" className="mt-1" onFinish={onFinish}>
         <Form.Item
           label="Name"
           name="name"
-          rules={[{ required: true, message: "Please Enter your name" }]}
-        >
+          rules={[{ required: true, message: "Please Enter your name" }]}>
           <input type="text" />
         </Form.Item>
         <Form.Item
           label="Email"
           name="email"
-          rules={[{ required: true, message: "Please Enter your email" }]}
-        >
+          rules={[{ required: true, message: "Please Enter your email" }]} >
           <input type="email" />
         </Form.Item>
         <Form.Item
           label="Password"
           name="password"
-          rules={[{ required: true, message: "Please Enter your password" }]}
-        >
+          rules={[{ required: true, message: "Please Enter your password" }]}  >
           <input type="password" />
         </Form.Item>
         <div className="flex flex-col mt-2 gap-1">
