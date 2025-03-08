f071d2ea07d9ec2764ceb86a00d4f6624a2036cd
Kishore <kishorekishore201@gmail.com>
2025-03-08 10:43:56 +0530

    salt and bcyrpt node modules and code

--- Server/controller/userController.js
+++ Server/controller/userController.js
@@ -1,8 +1,18 @@
 const userModel = require("../model/userModel");
+const bcrypt = require("bcrypt");
 
 const registerUser= async  (req,res)=>{
     try{
-      
+      const userExists = await userModel.findOne({email: req.body?.email});
+      if(userExists){
+        return res.status(200).json({
+          success:false,
+          message: "User Already Exists",
+        });
+      }
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(req.body?.password, salt);
+      req.body.password= hashedPassword;
         const user = new userModel(req.body);
         const response = await user.save();
 
