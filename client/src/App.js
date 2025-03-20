15fbc9c16b46afe0d9cfa6fc6286424292a86a12
Kishore <kishorekishore201@gmail.com>
2025-03-21 04:36:27 +0530

    Unwated class removed

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/Login" element={<Login/>}/>
         <Route path="/Register" element={<Register/>}/>
+       
       </Routes>
       </BrowserRouter >
     </div>
