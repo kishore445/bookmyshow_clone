7e8158c5e9ed5d28c5d50804278f96aad2226ddf
Kishore <kishorekishore201@gmail.com>
2025-07-26 10:49:48 +0530

    crud operations

--- Server/routes/movieRouter.js
+++ Server/routes/movieRouter.js
@@ -1,7 +1,10 @@
 const movieRouter = require('express').Router();
-const {AddMovie}= require('../controller/movieController')
+const {AddMovie,getAllMovies, updateMovies, deleteMovies}= require('../controller/movieController')
 
 
-movieRouter.post('/add-movie', AddMovie);
+movieRouter.post('/addMovie', AddMovie);
+movieRouter.get('/getAllMovies', getAllMovies);
+movieRouter.patch('/updateMovie', updateMovies);
+movieRouter.delete('/deleteMovie', deleteMovies)
 
 module.exports= movieRouter
